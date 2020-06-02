57cf52f3063ffb168f0f68237186a4a1c2e2e2fc
JaredMathis <jaredmathis@protonmail.com>
2020-06-02 13:07:33 -0400

    update

--- library/graphTraverse.js
+++ library/graphTraverse.js
@@ -10,6 +10,7 @@ function graphTraverse(graph) {
     let result;
     u.scope(graphTraverse.name, x => {
         let log = false;
+        if (log) console.log(graphTraverse.name + " entered");
         u.merge(x,{graph});
         u.assert(() => isGraph(graph));
 
@@ -19,6 +20,7 @@ function graphTraverse(graph) {
         let current = vertices[0];
         result = [current];
         let spice = 0;
+        let toggle = false;
         u.loop(u.range(vertices.length * vertices.length), i => {
             let neighbors = getGraphNeighbors(graph, current);
             if (u.isUndefined(previous)) {
@@ -34,7 +36,10 @@ function graphTraverse(graph) {
             let choiceIndex = spice % neighbors.length;
             if (log) console.log({spice,choiceIndex,neighbors,current});
             if (neighbors.length > 1) {
-                spice += 1;
+                toggle = !toggle;
+                if (toggle) {
+                    spice += neighbors.length - 1;
+                }
             }
             previous = current;
             current = neighbors[choiceIndex];
--- tests/graphTraverse/graphTraverse.js
+++ tests/graphTraverse/graphTraverse.js
@@ -9,11 +9,12 @@ u.scope(__filename, x => {
     graphTraverse([[2,3],[0,3],[1,3]]);
     graphTraverse([[1,3],[0,1],[0,2]]);
     graphTraverse([[0,3],[0,2],[0,1]]);
+    graphTraverse([[0,4],[2,4],[2,3],[1,2]]);
 
-    let trials = 100;
-    let max = 5;
+    let trials = 1000;
+    let max = 4;
     let start = 3;
-    u.loop(u.range(max - start, start), vertexCount => {
+    u.loop(u.range(max - start + 1, start), vertexCount => {
         u.loop(u.range(trials), () => {
             let graph = generateGraphTree(vertexCount);
             graphTraverse(graph);
